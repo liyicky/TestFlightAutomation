d4692f538a749e2e936dbe312a04b950003e6200
liyicky <liyicky@gmail.com>
2014-03-11 09:42:55 -0400

    Updated script to use rbenv, plus cleanup

--- build.sh
+++ build.sh
@@ -3,18 +3,21 @@
 # set -e exits the terminal once a command returns a non 0 exit code. Use this to not push any broken code.
 set -e
 
-HOME=""
+# Paths
 PROJECT_PATH=""
 PROJECT_NAME=""
 RELEASE_BUILD_PATH=""
 IPA_PATH="${PROJECT_PATH}/${PROJECT_NAME}.ipa"
 DSYM_ZIP="${PROJECT_PATH}/${PROJECT_NAME}.app.dSYM.zip"
-DEVELOPER=""
 PROVISIONING_PROFILE=""
+
+# TestFlight Vars
+DEVELOPER=""
 API_TOKEN="abc"
 TEAM_TOKEN="123"
-DATE=$(/bin/date +%"Y-%m-%d")
 APP="${HOME}/Library/Developer/Xcode/Archives/${DATE}/${ARCHIVE}/Products/Applications/${PROJECT_NAME}.app"
+DISTRIBUTION_LIST="Your_TestFlight_Teams"
+DATE=$(/bin/date +%"Y-%m-%d")
 
 
 # Clean up
@@ -26,47 +29,13 @@ APP="${HOME}/Library/Developer/Xcode/Archives/${DATE}/${ARCHIVE}/Products/Applic
 NOTES=$(/bin/cat /tmp/testflightnotes.txt)
 
 
-#echo "Building...#
-/usr/bin/cd $PROJECT_PATH
-git checkout develop
-git pull origin develop
+# Git the latest version
+git checkout YOUR_BRANCH 
+git fetch
+git reset --hard remotes/origin/AppOrchard
 
-git checkout deployment
-git merge develop
-
-
-# Get the current version
-version=$(agvtool what-version)
-version=$(echo $version | awk -F "." '{print $1, $2, $3, $4, $5, $6, $7, $8, $9}' )
-set -- $version
-#
-# Split the version numbers 
-export v1=$7
-export v2=$8
-export v3=$9
-#
-# Increment version
-v3=$((v3+1))
-if [ $v3 -gt 9 ]
-then
-  v3=0
-  v2=$((v2+1))
-fi
- 
-if [ $v2 -gt 9 ]
-then
-  v2=0
-  v1=$((v1+1))
-fi
- 
-NEW_VERSION="$v1.$v2.$v3"
-agvtool new-version -all $NEW_VERSION
-/opt/boxen/rbenv/shims/ipa build
-
-# Commit new version and push
-git add .
-git commit -m "Build $NEW_VERSION"
-git push origin deployment
+# Build
+$HOME/.rbenv/shims/ipa build --verbose --scheme YOUR_SCHEME
 
 # curl everything to TestFlight
 /usr/bin/curl "http://testflightapp.com/api/builds.json" \
@@ -74,8 +43,10 @@ git push origin deployment
   -F dsym=@"${DSYM_ZIP}"        \
   -F api_token="${API_TOKEN}"   \
   -F team_token="${TEAM_TOKEN}" \
-  -F notes="${NOTES}"
+  -F notify=True                \
+  -F notes="${NOTES}"           \
+  -F distribution_list="${DISTRIBUTION_LIST}"
 
 git wood --summary HEAD^..HEAD
 /usr/bin/open "https://testflightapp.com/dashboard/builds/"
-/opt/boxen/rbenv/shims/terminal-notifier -title "${PROJECT_NAME}" -subtitle "New Build ${NEW_VERSION}" -message "${NOTES}" 
+$HOME/.rbenv/shims/terminal-notifier -title "${PROJECT_NAME}" -subtitle "New Build ${NEW_VERSION}" -message "${NOTES}" 
